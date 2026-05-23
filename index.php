0c0d03bd26da7f4d50bccfcb40fd6d41111f137d
Will-2207 <cartuji7@gmail.com>
2026-05-22 21:07:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 //$conn = new mysqli($servername, $username, $password, $dbname);
 //if ($conn->connect_error) {
    // $error_db = "Error de conexión: " . $conn->connect_error;
-}
+//}
 //$conn->set_charset("utf8mb4");
 ?>
 
