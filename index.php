f898de3616d3c06f07446dddff89998188d38c9d
Will-2207 <cartuji7@gmail.com>
2026-05-22 21:04:53 -0500

    Disable database connection and error handling
    Comment out database connection code and error handling

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 //$dbname     = "if0_41748799_donaciones";
 
 //$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    $error_db = "Error de conexión: " . $conn->connect_error;
+//if ($conn->connect_error) {
+   // $error_db = "Error de conexión: " . $conn->connect_error;
 }
-$conn->set_charset("utf8mb4");
+//$conn->set_charset("utf8mb4");
 ?>
 
 <!DOCTYPE html>
